e735d3ca0f0728f0d73b9f9e99a279175ab3203c
subhani <subhanis.2017@gmail.com>
2023-12-30 16:25:44 +0530

    Linear Search Code

--- demo.py
+++ demo.py
@@ -1,3 +1,19 @@
-print("Hello")
-print("Hello World")
-print("Last Changes")
+pos = -1
+
+
+def search(plist, pn):
+    i = 0
+    while i < len(plist):
+        if plist[i] == pn:
+            globals()['pos'] = i
+            return True
+        i = i + 1
+    return False
+
+
+new_list = [10, 20, 30, 40, 22]
+n = 30
+if search(new_list, n):
+    print("Found", pos + 1)
+else:
+    print("Not Found")
